83c0d7f754a27e988d0d05dbf30d663c0592b513
Anand Verma <anandverma552@gmail.com>
2024-05-04 17:21:32 -0400

    Using LLM chain for input formating

--- src/modules/LangchainModule.js
+++ src/modules/LangchainModule.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { OpenAI } from "@langchain/openai";
+import { LLMChain } from "langchain/chains";
 import { PromptTemplate } from "@langchain/core/prompts";
 
 dotenv.config();
@@ -22,17 +23,18 @@ export default class LangChainModule {
   }
 
   async promptTemplate(prompt) {
-    const firstTemplate = "What would be a good shop name for a business that seels {product}? Give me a few options.";
+    const firstTemplate =
+      "What would be a good shop name for a business that seels {product}? Give me a few options.";
 
     const promptTemplate = new PromptTemplate({
-        template: firstTemplate,
-        inputVariables: ["product"],    
-    })
+      template: firstTemplate,
+      inputVariables: ["product"],
+    });
 
     const formattedPrompt = await promptTemplate.format({
-        product: prompt
-    })
-    console.log(formattedPrompt)
+      product: prompt,
+    });
+    console.log(formattedPrompt);
     try {
       const response = await this.model.call(formattedPrompt);
       console.log(response);
@@ -41,4 +43,29 @@ export default class LangChainModule {
     }
   }
 
+  async promptTemplateChain(promt) {
+    const firstTemplate =
+      "What would be a good shop name for a business that seels {product}? Give me a few options.";
+    const promptTemplate = new PromptTemplate({
+      template: firstTemplate,
+      inputVariables: ["product"],
+    });
+
+    const chain = new LLMChain({
+      llm: this.model,
+      prompt: promptTemplate
+    })
+
+    try {
+      const response = await chain.call({
+        product: promt
+      })
+
+      console.log(response.text)
+    } catch(err) {
+      console.error("Error", err)
+    }
+
+
+  }
 }
